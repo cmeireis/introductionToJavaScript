283a3b8c187b28bd7548e2336ecaf745b1491101
Cody Meireis <codymeireis@gmail.com>
2020-04-30 20:45:54 -0500

    adding exercise 2 age.js

---
+++ Variables/age.js
@@ -0,0 +1,6 @@
+let myAge = 37;
+console.log(`You are ${ myAge } years old.`);
+console.log(`In 10 years, you will be ${ myAge + 10} years old.`);
+console.log(`In 20 years, you will be ${ myAge + 20} years old.`);
+console.log(`In 30 years, you will be ${ myAge + 30} years old.`);
+console.log(`In 40 years, you will be ${ myAge + 40} years old.`);
