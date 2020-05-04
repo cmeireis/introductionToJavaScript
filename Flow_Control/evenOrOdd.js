67c2e624b97f9c0e599c62b111380056960abe3b
cody.meireis@kochind.com <cody.meireis@kochind.com>
2020-05-04 16:26:01 -0500

    adding oddeven function

--- Flow_Control/evenOrOdd.js
+++ Flow_Control/evenOrOdd.js
@@ -1,10 +1,10 @@
-let number = 3
+//let number = 2;
 
 function evenOrOdd(number) {
 if (number % 2 == 0) {
   console.log('Your number is even.');
 } else {
   console.log('Your number is odd');
-};
+}};
 
-evenOrOdd();
+evenOrOdd(2);
