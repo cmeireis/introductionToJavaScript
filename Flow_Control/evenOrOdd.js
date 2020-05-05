242aba9b159ba4c4cbf26686c90538a176d7984a
cody.meireis@kochind.com <cody.meireis@kochind.com>
2020-05-05 10:26:34 -0500

    updating odd.js

--- Flow_Control/evenOrOdd.js
+++ Flow_Control/evenOrOdd.js
@@ -1,10 +1,12 @@
 //let number = 2;
 
 function evenOrOdd(number) {
-if (number % 2 == 0) {
+if (Number.isInteger(number) && number % 2 == 0) {
   console.log('Your number is even.');
-} else {
+} else if ((Number.isInteger(number)) && (number % == 0)) {
   console.log('Your number is odd');
-}};
+} else {
+  console.log('Error: Number is not an integer!')
+}}};
 
 evenOrOdd(2);
