fe5518b4f6e29309e29074d202d10c8aa74c58c7
Cody Meireis <codymeireis@Codys-MacBook-Air.local>
2020-05-09 00:14:42 -0500

    add

---
+++ Arrays/arrayexercises.js
@@ -0,0 +1,46 @@
+/*let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+
+myArray.forEach(function(numbers) {
+  if (numbers % 2 === 0) {
+    console.log(numbers);
+  }
+});*/
+
+// exercise 3
+/*let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+myArray.map(function(numbers) {
+  return numbers.map(function(number) {
+    if (number % 2 === 0) {
+      console.log(number);
+    }
+  })
+});*/
+
+// exercise 4
+
+/*let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+let newArray = myArray.map(function(number) {
+  if (number % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+});
+console.log(newArray);*/
+
+// exercise 5
+/*let array = [1, 'a', '1', 3, NaN, 3.145, -4, null, false];
+
+function notIntegers(arrays) {
+  return array.filter(integer => Number.isInteger(integer));
+}
+console.log(notIntegers(array));*/
+
+// exercise 6
+
+
