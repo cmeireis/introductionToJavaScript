af505ae272f1c5c3facaa7e17ad1092bd716353a
Cody Meireis <codymeireis@Codys-MacBook-Air.local>
2020-05-11 08:12:03 -0500

    adding exercises 1 through 3 for object chapter

---
+++ Objects/object_exercises.js
@@ -0,0 +1,21 @@
+// exercise 1
+/*let person = {
+  name:       'Bob',
+  occupation: 'web developer',
+  hobbies:    'painting',
+};
+
+console.log(person.name);*/
+
+// exercise 3
+
+let myArray = {
+  0: 'apple',
+  1: 'orange',
+  2: 'banana',
+  length: 3
+};
+
+for (let i = 0; i < myArray.length; i += 1) {
+  console.log(myArray[i]);
+};
